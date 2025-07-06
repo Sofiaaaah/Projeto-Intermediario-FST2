31e169116cfac0505d677924f88694f51620df1d
Sofia <Sofiaabrs1425@gmail.com>
2025-07-06 17:38:04 -0300

    atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Input from './components/Input'
 function App() {
   return (
     <>
+    <div>
+      <h2>Bem vindo</h2>
+    </div>
       <Input id="produto" placeholder="Produto" />
       <Input id="quantidade" placeholder="Quantidade" />
     </>
