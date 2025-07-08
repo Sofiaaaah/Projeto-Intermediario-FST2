cd522bc40dcfc8b1777fa7fc027d2583e1f3ff16
Sofia <Sofiaabrs1425@gmail.com>
2025-07-07 21:06:07 -0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import Input from './components/Input'
+import './assets/css/main.css';
 
 function App() {
   return (
-    <>
-    <div>
-      <h2>Bem vindo</h2>
+    <div id='caixa' className='font-bold text-center'>
+
+      <h1 className='text-gray-500 font-bold'>Lista de Compras e Quantidades</h1>
+
+      <div id="linha-inputs">
+        <Input id="produto" placeholder="Nome Produto" />
+        <Input id="quantidade" placeholder="Quantidade" />
+        <button id="adicionar" className="bg-red-300 text-white">
+        Adicionar
+        </button>
+      </div>
+      
+
     </div>
-      <Input id="produto" placeholder="Produto" />
-      <Input id="quantidade" placeholder="Quantidade" />
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
