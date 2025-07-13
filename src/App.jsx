00018de70804339d685dc1b7792a85d178f11038
Sofia <Sofiaabrs1425@gmail.com>
2025-07-12 21:14:58 -0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,78 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import './assets/css/style.css';
+
 function App() {
     const [itens, setItens] = useState([]);
     const [produto, setProduto] = useState("");
     const [quantidade, setQuantidade] = useState("");
+
+    Storage.prototype.setObj = (key, obj) => {
+        return this.setItem(key, JSON.stringify(obj))
+    }
+
+    Storage.prototype.getObj = (key) => {
+        return JSON.parse(this.getItem(key))
+    }
 
     const adicionar = () => {
         if (produto && quantidade > 0) {
             setItens([...itens, { produto, quantidade }]);
             setProduto("");
             setQuantidade("");
+            localStorage.setObj("todo-itens", itens);
         }
     };
 
     const remover = (vazio) => {
         setItens(itens.filter((_, final) => final != vazio));
+        localStorage.setObj("todo-itens", itens.values);
     };
 
+    const removerTodos = () => {
+        setItens([]);
+        localStorage.removeItem("todo-itens");
+    };
+
+    useEffect(() => {
+        setItens(localStorage.getObj("todo-itens"))
+    }, [itens]);
+
     return (
-        <div id="body">
-            <div id="caixa">
-                <div id="navegador">
+        <div id="caixa">
+            <div id="navegador">
+                <h1>📝 Vamos adicionar !</h1>
 
-                    <h1>🐥Lista de Ajuda🐥</h1>
+                <input
+                    type="text"
+                    id="produto"
+                    placeholder="Produto"
+                    value={produto}
+                    onChange={e => setProduto(e.target.value)}
+                />
+                <input
+                    id="quantidade"
+                    type="number"
+                    placeholder="Quantidade"
+                    value={quantidade}
+                    onChange={e => setQuantidade(e.target.value)}
+                />
+                <button onClick={adicionar} id="adicionar">Adicionar</button>
+            </div>
 
-                    <input
-                        type="text"
-                        id="produto"
-                        placeholder="Produto"
-                        value={produto}
-                        onChange={sla => setProduto(sla.target.value)}
-                    />
-                    <input
-                        id="quantidade"
-                        type="number"
-                        placeholder="Quantidade"
-                        value={quantidade}
-                        onChange={sla => setQuantidade(sla.target.value)}
-                    />
-                    <button onClick={adicionar} id="adicionar">Adicionar</button>
-                </div>
-
-
+            <div>
                 <ul>
-                    {itens.map((item, vazio) => (
-                        <p id="resultado">
-                            <div id="Rproduto">
-                               {item.produto}   
-                            </div>  
-                            <div id="Rquantidade">
-                               {item.quantidade}   
-                            </div>
-                         
-                             
-                            <button onClick={() => remover(vazio)} id="remover">Remover</button>
+                    {itens.map((item, index) => (
+                        <p class="resultado" key={index}>
+                            <div class="Rproduto">{item.produto}</div>
+                            <div class="Rquantidade">{item.quantidade}</div>
+                            <button onClick={() => remover(index)} class="remover">Remover</button>
                         </p>
                     ))}
                 </ul>
-                <button id="resetar" onClick={()=>setItens([])}>resetar</button>
+            </div>
+
+            <div>
+                <button id="resetar" onClick={() => removerTodos()}>resetar</button>
             </div>
         </div>
 
