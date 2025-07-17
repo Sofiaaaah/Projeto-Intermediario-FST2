f93254c9eb0505be9cb502e925c65fbafb9738a2
Sofia <Sofiaabrs1425@gmail.com>
2025-07-16 22:27:38 -0300

    feat: animation css

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,90 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-import './assets/css/style.css'
+import './assets/css/style.css';
 
 const carregarItens = () => {
-  return JSON.parse(localStorage.getItem('todo-itens')) || []
-}
+  return JSON.parse(localStorage.getItem('todo-itens')) || [];
+};
 
 function App() {
-  const [itens, setItens] = useState(() => carregarItens())
-  const [produto, setProduto] = useState('')
-  const [quantidade, setQuantidade] = useState('')
+  const [itens, setItens] = useState(() => carregarItens());
+  const [produto, setProduto] = useState('');
+  const [quantidade, setQuantidade] = useState('');
+  const [mensagem, setMensagem] = useState(''); // estado para mensagem
 
   const adicionarItem = () => {
+    // valida produto e quantidade > 0
     if (produto && quantidade > 0) {
-      setItens([...itens, { produto, quantidade }])
-      setProduto('')
-      setQuantidade('')
+      const novoItem = { produto, quantidade };
+      setItens([...itens, novoItem]);
+      setProduto('');
+      setQuantidade('');
+
+      setMensagem('Produto adicionado com sucesso!');
+      setTimeout(() => setMensagem(''), 1000);
     }
-  }
+  };
 
   const removerItem = (index) => {
-    setItens(itens.filter((_, final) => final != index))
-  }
+    setItens(itens.filter((_, i) => i !== index));
+  };
 
   const removerTodosItens = () => {
-    setItens([])
-  }
+    setItens([]);
+  };
 
-  const salvarItens = () => {
-    localStorage.setItem('todo-itens', JSON.stringify(itens))
-  }
+  useEffect(() => {
+    localStorage.setItem('todo-itens', JSON.stringify(itens));
+  }, [itens]);
 
-  useEffect(salvarItens, [itens])
+  return (
+    <div id="caixa">
+      <h1>🥕 O que não pode faltar Hoje?</h1>
+      <div id="navegador">
+        <input
+          type="text"
+          id="produto"
+          placeholder="Produto"
+          value={produto}
+          onChange={(e) => setProduto(e.target.value)}
+        />
+        <input
+          id="quantidade"
+          type="number"
+          placeholder="Quantidade"
+          value={quantidade}
+          onChange={(e) => setQuantidade(e.target.value)}
+        />
+        <button onClick={adicionarItem} id="adicionar">
+          Adicionar
+        </button>
+      </div>
 
-    return (
-        <div id="caixa">
-            <div id="navegador">
-                <h1>📝 Vamos adicionar !</h1>
-
-                <input
-                    type="text"
-                    id="produto"
-                    placeholder="Produto"
-                    value={produto}
-                    onChange={e => setProduto(e.target.value)}
-                />
-                <input
-                    id="quantidade"
-                    type="number"
-                    placeholder="Quantidade"
-                    value={quantidade}
-                    onChange={e => setQuantidade(e.target.value)}
-                />
-                <button onClick={adicionar} id="adicionar">Adicionar</button>
-            </div>
+      {/* MENSAGEM DE PROD ADICIONADO*/}
+      {mensagem && (
+        <p id="mensagem-prod">
+          {mensagem}
+        </p>
+      )}
 
       <div>
         <ul>
           {itens.map((item, index) => (
             <div className="resultado" key={index}>
-              <input className="resultado-produto" value={item.produto} readOnly />
-              <input className="resultado-quantidade" value={item.quantidade} readOnly />
-              <button onClick={() => removerItem(index)} className="resultado-btn-remover">
+              <input
+                className="resultado-produto"
+                value={item.produto}
+                readOnly
+              />
+              <input
+                className="resultado-quantidade"
+                value={item.quantidade}
+                readOnly
+              />
+              <button
+                onClick={() => removerItem(index)}
+                className="resultado-btn-remover"
+              >
                 Remover
               </button>
             </div>
@@ -70,12 +93,12 @@ function App() {
       </div>
 
       <div>
-        <button id="resetar" onClick={() => removerTodosItens()}>
-          resetar
+        <button id="resetar" onClick={removerTodosItens}>
+          🔄 Resetar lista
         </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
